0c12c33957e44eddb54868a1e64ac91bdfcd4f3e
G V Pranav Sharma <pranav.23bey10032@vitbhopal.ac.in>
2025-07-17 00:15:12 +0530

    Linked List Display

--- Linkedlist.cpp
+++ Linkedlist.cpp
@@ -93,5 +93,41 @@ struct Node
 {
     int data;
     Node *next;
-    Node(int data = 0) : data(data), next(nullptr) {}
-};
+} *first = NULL;
+
+void create(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    if (n <= 0)
+        return;
+    first = new Node;
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    for (i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+void Display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    int A[] = {3, 5, 7, 10, 15};
+    create(A, 5);
+    Display(first);
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////
